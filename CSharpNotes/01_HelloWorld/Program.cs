d7c5980922ab88d42eca76e2e30591c4eeb12f5c
Eren KADİROĞLU <154251984+erenkadiroglu@users.noreply.github.com>
2025-07-17 23:40:43 +0300

    Project added.
    The first topic, Hello World, has been added.

---
+++ CSharpNotes/01_HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello World! ");   //Prints text on the screen
+            Console.WriteLine("I'm Eren.");   //Prints text on the screen and moves to the next line
+            Console.ReadKey(); //Prevents the program from ending
+        }
+    }
+}
